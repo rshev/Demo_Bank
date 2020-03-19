8e653662ca9fc77395eed3f3273be5633ac57d80
Roman Shevtsov <roman.shevtsov@gmail.com>
2020-03-19 19:19:29 +0000

    Initial Commit

---
+++ StarlingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarlingTest
+//
+//  Created by asdfgh1 on 19/03/2020.
+//  Copyright © 2020 Roman Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
